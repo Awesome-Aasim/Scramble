6fa4d443c744387c11b3c8ce7285d109d99efcfe
Aasim <65820963+Awesome-Aasim@users.noreply.github.com>
2020-09-23 11:41:10 -0700

    work on screen capture and the like

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scramble
+{
+    public partial class Scramble : Form
+    {
+        public enum GWL
+        {
+            ExStyle = -20
+        }
+
+        public enum WS_EX
+        {
+            Transparent = 0x20,
+            Layered = 0x80000
+        }
+
+        public enum LWA
+        {
+            ColorKey = 0x1,
+            Alpha = 0x2
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        const UInt32 SWP_NOSIZE = 0x0001;
+
+        const UInt32 SWP_NOMOVE = 0x0002;
+
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+
+
+        [DllImport("user32.dll")]
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        public static extern int SetWindowLong(IntPtr hWnd, GWL nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
+        public static extern bool SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte alpha, LWA dwFlags);
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            int wl = GetWindowLong(this.Handle, GWL.ExStyle);
+            wl = wl | 0x80000 | 0x20;
+            SetWindowLong(this.Handle, GWL.ExStyle, wl);
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+            //SetLayeredWindowAttributes(this.Handle, 0, 128, LWA.Alpha);
+        }
+        public Scramble()
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor;
+            this.Size = new Size(Screen.FromControl(this).Bounds.Width, Screen.FromControl(this).Bounds.Height);
+        }
+        private void Scramble_Load(object sender, EventArgs e)
+        {
+            label1.Location = new Point(Screen.FromControl(this).Bounds.Width / 2, Screen.FromControl(this).Bounds.Height / 2);
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+        private static Bitmap Screenshot()
+        {
+            SetProcessDPIAware();
+            var screen = System.Windows.Forms.Screen.PrimaryScreen;
+            var rect = screen.Bounds;
+            var size = rect.Size;
+
+            Bitmap bmpScreenshot = new Bitmap(size.Width, size.Height);
+            Graphics g = Graphics.FromImage(bmpScreenshot);
+            g.CopyFromScreen(0, 0, 0, 0, size);
+
+            return bmpScreenshot;
+        }
+    }
+}
